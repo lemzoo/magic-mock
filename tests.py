1ffda992f8a2d067c5039d1c045c872e17c710b2
lemzoo <lemzobba@gmail.com>
2017-03-03 00:05:42 +0100

    Cleaning the file

--- tests.py
+++ tests.py
@@ -3,42 +3,43 @@ from unittest.mock import Mock
 
 from tools import Calculette, CacluletteScientifique
 
+
 class TestWithoutMock:
-	def setup(self):
-		self.calculette = Calculette()
+    def setup(self):
+        self.calculette = Calculette()
 
-	def test_add(self):
-		expected_result = 3 + 4
-		returned_result = self.calculette.add(3, 4)
-		assert returned_result == expected_result
+    def test_add(self):
+        expected_result = 3 + 4
+        returned_result = self.calculette.add(3, 4)
+        assert returned_result == expected_result
 
-	def test_sub(self):
-		expected_result = 3 - 4
-		returned_result = self.calculette.sub(3, 4)
-		assert returned_result == expected_result
+    def test_sub(self):
+        expected_result = 3 - 4
+        returned_result = self.calculette.sub(3, 4)
+        assert returned_result == expected_result
 
-	def test_mul(self):
-		expected_result = 3 * 4
-		returned_result = self.calculette.mul(3, 4)
-		assert returned_result == expected_result
+    def test_mul(self):
+        expected_result = 3 * 4
+        returned_result = self.calculette.mul(3, 4)
+        assert returned_result == expected_result
 
-	def test_div(self):
-		expected_result = 3 / 4
-		returned_result = self.calculette.div(3, 4)
-		assert returned_result == expected_result
+    def test_div(self):
+        expected_result = 3 / 4
+        returned_result = self.calculette.div(3, 4)
+        assert returned_result == expected_result
 
 
 class TestWithMock(TestWithoutMock):
-	def test_add(self):
-		self.calculette.add = Mock()
-		self.calculette.add(3, 4)
-		self.calculette.add.assert_called_with(3, 4)
+    def test_add(self):
+        self.calculette.add = Mock()
+        self.calculette.add(3, 4)
+        self.calculette.add.assert_called_with(3, 4)
 
-	def test_sub(self):
-		self.calculette.sub = Mock(return_value=(3-4))
-		returned = self.calculette.sub(3, 4)
-		expected = self.calculette.sub.return_value
-		assert returned == expected
+    def test_sub(self):
+        self.calculette.sub = Mock(return_value=(3-4))
+        returned = self.calculette.sub(3, 4)
+        expected = self.calculette.sub.return_value
+        assert returned == expected
 
 
 class TestFullMock:
--- tools.py
+++ tools.py
@@ -1,46 +1,43 @@
 #!/usr/bin/env python
 
-def add(first_arg, second_arg):
-	return first_arg + second_arg
-
 
 def add(first_arg, second_arg):
-	return first_arg + second_arg
+    return first_arg + second_arg
 
 
 def sub(first_arg, second_arg):
-	return first_arg - second_arg
+    return first_arg - second_arg
 
 
 def mul(first_arg, second_arg):
-	return first_arg * second_arg
+    return first_arg * second_arg
 
 
 def div(first_arg, second_arg):
-	return first_arg / second_arg
+    return first_arg / second_arg
 
 
 class Calculette:
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def add(self, first_arg, second_arg):
-		return add(first_arg,second_arg)
+    def add(self, first_arg, second_arg):
+        return add(first_arg, second_arg)
 
-	def sub(self, first_arg, second_arg):
-		return sub(first_arg, second_arg)
+    def sub(self, first_arg, second_arg):
+        return sub(first_arg, second_arg)
 
-	def mul(self, first_arg, second_arg):
-		return mul(first_arg, second_arg)
+    def mul(self, first_arg, second_arg):
+        return mul(first_arg, second_arg)
 
-	def div(self, first_arg, second_arg):
-		return div(first_arg, second_arg)
+    def div(self, first_arg, second_arg):
+        return div(first_arg, second_arg)
 
 
 class CacluletteScientifique:
-	def __init__(self, calculette):
-		self.calculette = calculette
+    def __init__(self, calculette):
+        self.calculette = calculette
 
-	def carre(self, value):
-		carre = self.calculette.mul(value, value)
-		return carre
+    def carre(self, value):
+        carre = self.calculette.mul(value, value)
+        return carre
